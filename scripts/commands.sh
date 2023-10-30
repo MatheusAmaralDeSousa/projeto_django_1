56d21856727fd85df155f0485c8b5228c74ec9a2
MatheusAmaralDeSousa <theusamarausyt@gmail.com>
2023-10-30 20:28:37 -0300

    fix: docker and commands.sh settings

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,7 +5,7 @@ set -e
 
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-  sleep 2
+  sleep 5
 done
 
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
