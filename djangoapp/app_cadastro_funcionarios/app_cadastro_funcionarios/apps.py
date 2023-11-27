5f711b4df27a82b7e4736fbb75b2bc1819e66338
Matheus Amaral De Sousa <theusamarausyt@gmail.com>
2023-11-27 20:41:21 -0300

    refactor: urls and templates

--- djangoapp/app_cadastro_funcionarios/app_cadastro_funcionarios/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCadastroFuncionariosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_cadastro_funcionarios'
--- djangoapp/projeto/urls.py
+++ djangoapp/projeto/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('app_navbar.urls')),
     path('cadastro_cliente/',include('app_cadastro_usuarios.urls')),
-    path('cadastro_fornecedor/',include('app_cadastro_fornecedores.urls'))
+    path('cadastro_fornecedor/',include('app_cadastro_fornecedores.urls')),
+    path('cadastro_funcionario/',include('app_cadastro_fornecedores.urls')),
 ]
 
 
