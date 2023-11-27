e4927eaef9efed37ea81ed067de0b06a7e5500e7
Venícius <veniciusdobler840@gmail.com>
2023-11-27 02:02:59 -0300

    feat: viewa alterada

--- djangoapp/app_cadastro_fornecedores/views.py
+++ djangoapp/app_cadastro_fornecedores/views.py
@@ -1,27 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404, redirect
+from .models import Fornecedor
+from .forms import FornecedorForm, FornecedorEditForm
 
-from djangoapp.app_cadastro_fornecedores.models import Fornecedores
-from .models import Funcionarios
+def home_cadastro_fornecedor(request):
+    return render(request, 'home/home.html')
 
-def home(request):
-    return render(request,'fornecedores/home.html')
 
-def fornecedores(request):
-    #Salvar os dados da tela para o banco de dados
-    novo_fornecedor = Fornecedores()
-    novo_fornecedor.nome =  request.POST.get("nome")
-    novo_fornecedor.email =  request.POST.get("email")
-    novo_fornecedor.telefone =  request.POST.get("telefone")
-    novo_fornecedor.celular =  request.POST.get("celular")
-    novo_fornecedor.cep =  request.POST.get("cep")
-    novo_fornecedor.endereco =  request.POST.get("endereco")
-    novo_fornecedor.numero =  request.POST.get("numero")
-    novo_fornecedor.uf =  request.POST.get("uf")
-    novo_fornecedor.save()
-    
+def cadastro_fornecedor(request):
+    if request.method == 'POST':
+        form = FornecedorForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return redirect('listagem_fornecedor')
+    else:
+        form = FornecedorForm()
+
+    return render(request, 'fornecedores/cadastro_fornecedor.html', {'form': form})
+
+
+def listagem_fornecedor(request):
     #Exibir todos os fornecedores do banco de dados
-    fornecedor = {
-        'fornecedor' : Fornecedores.objects.all()
+    fornecedores = {
+        'fornecedores' : Fornecedor.objects.all().order_by('id_fornecedor')
     }
     #Retornar os dados para a pagina de listagem de fornecedores
-    return render(request, 'fornecedores/fornecedores.html', fornecedor)
+    return render(request, 'fornecedores/listagem_fornecedor.html', fornecedores)
+
+
+
+def editar_fornecedor(request, fornecedor_id):
+    fornecedor = get_object_or_404(Fornecedor, id_fornecedor=fornecedor_id)  
+
+    if request.method == 'POST':
+        form = FornecedorEditForm(request.POST, instance=fornecedor)
+        if form.is_valid():
+            form.save()
+            return redirect('listagem_fornecedor')
+    else:
+        form = FornecedorEditForm(instance=fornecedor) 
+
+    return render(request, 'fornecedores/editar_fornecedor.html', {'form': form, 'fornecedor_id': fornecedor_id})
+
+
+def delete_user(request, user_id):
+    fornecedor = get_object_or_404(Fornecedor, pk=user_id)
+    fornecedor.delete()
+    return redirect('listagem_fornecedor')
