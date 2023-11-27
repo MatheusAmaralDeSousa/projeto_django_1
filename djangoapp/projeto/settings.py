505e6662f00963aec8af62057124a48a6708474c
Matheus Amaral De Sousa <theusamarausyt@gmail.com>
2023-11-27 01:22:55 -0300

    feat: app_navbar registred

--- djangoapp/projeto/settings.py
+++ djangoapp/projeto/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_cadastro_usuarios'
+    'app_cadastro_usuarios',
+    'app_navbar'
 ]
 
 MIDDLEWARE = [
