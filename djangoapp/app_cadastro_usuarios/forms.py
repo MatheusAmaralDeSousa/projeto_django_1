dbfbfadf4c73bb9ad7f906424f711d39c044ee9a
Venícius <veniciusdobler840@gmail.com>
2023-11-27 14:54:30 -0300

    refactor: models and forms changed

--- djangoapp/app_cadastro_usuarios/forms.py
+++ djangoapp/app_cadastro_usuarios/forms.py
@@ -1,12 +1,69 @@
 from django import forms
 from .models import Usuario
+from django.core.validators import EmailValidator, RegexValidator
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ["nome", "idade", "cpf", "rg"]
+        fields = ["nome", "idade", "cpf", "rg" "email", "telefone", "cep", "endereco", "bairro", "cidade", "complemento", "numero", "uf"]
+    
+    idade = forms.IntegerField(
+        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
+    )
+
+    cpf = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{11}$', message='O CPF deve conter exatamente 11 dígitos.')]
+    )
+
+    email = forms.CharField(
+        validators=[EmailValidator(message='Informe um endereço de e-mail válido.')]
+    )
+
+    telefone = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{9,11}$', message='Informe um número de telefone válido.')]
+    )
+
+    cep = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{8}$', message='O CEP deve conter exatamente 8 dígitos.')]
+    )
+
+    numero = forms.IntegerField(
+        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
+    )
+    
+    UF = forms.CharField(
+        validators=[RegexValidator(regex=r'^[A-Za-z]{2}$', message='A UF deve conter exatamente 2 letras.')]
+    )
 
 class UsuarioEditForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ["nome", "idade", "cpf", "rg"]
+        fields = ["nome", "idade", "cpf", "rg" "email", "telefone", "cep", "endereco", "bairro", "cidade", "complemento", "numero", "uf"]
+    
+    idade = forms.IntegerField(
+        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
+    )
+
+    cpf = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{11}$', message='O CPF deve conter exatamente 11 dígitos.')]
+    )
+
+    email = forms.CharField(
+        validators=[EmailValidator(message='Informe um endereço de e-mail válido.')]
+    )
+
+    telefone = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{9,11}$', message='Informe um número de telefone válido.')]
+    )
+
+    cep = forms.CharField(
+        validators=[RegexValidator(regex=r'^\d{8}$', message='O CEP deve conter exatamente 8 dígitos.')]
+    )
+
+    numero = forms.IntegerField(
+        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
+    )
+    
+    UF = forms.CharField(
+        validators=[RegexValidator(regex=r'^[A-Za-z]{2}$', message='A UF deve conter exatamente 2 letras.')]
+    )
