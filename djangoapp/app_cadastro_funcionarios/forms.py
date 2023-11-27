8a3422f35bea492e934181c9e58f74a3dee317cb
Venícius <veniciusdobler840@gmail.com>
2023-11-27 16:00:00 -0300

    refactor: forms changed

--- djangoapp/app_cadastro_funcionarios/forms.py
+++ djangoapp/app_cadastro_funcionarios/forms.py
@@ -9,63 +9,106 @@ class FuncionarioForm(forms.ModelForm):
         model = Funcionario
         fields = ["nome", "idade", "cpf", "rg", "email", "telefone", "cep", "endereco", "bairro", "cidade", "complemento", "numero", "uf", "cargo"]
     
-    idade = forms.IntegerField(
-        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
-    )
+    # Validação de idade
+    def clean_idade(self):
+        idade = self.cleaned_data.get('idade')
+        if idade < 18:
+            raise ValidationError('A idade deve ser maior ou igual a 18 anos.')
+        return idade
 
-    cpf = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{11}$', message='O CPF deve conter exatamente 11 dígitos.')]
-    )
+    # Validação de CPF
+    def clean_cpf(self):
+        cpf = self.cleaned_data.get('cpf')
+        if not cpf.isdigit() or len(cpf) != 11:
+            raise ValidationError('CPF inválido. Deve conter apenas números e ter 11 dígitos.')
+        return cpf
 
-    email = forms.CharField(
-        validators=[EmailValidator(message='Informe um endereço de e-mail válido.')]
-    )
+    # Validação de RG
+    def clean_rg(self):
+        rg = self.cleaned_data.get('rg')
+        if not rg.isdigit() or len(rg) != 9:
+            raise ValidationError('RG inválido. Deve conter apenas números e ter 9 dígitos.')
+        return rg
 
-    telefone = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{9,11}$', message='Informe um número de telefone válido.')]
-    )
 
-    cep = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{8}$', message='O CEP deve conter exatamente 8 dígitos.')]
-    )
+    # Validação de e-mail
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_validator = EmailValidator('E-mail inválido.')
+        email_validator(email)
+        return email
 
-    numero = forms.IntegerField(
-        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
-    )
-    
-    UF = forms.CharField(
-        validators=[RegexValidator(regex=r'^[A-Za-z]{2}$', message='A UF deve conter exatamente 2 letras.')]
-    )
+    # Validação de telefone
+    def clean_telefone(self):
+        telefone = self.cleaned_data.get('telefone')
+        if not telefone.isdigit() or len(telefone) < 9:
+            raise ValidationError('Telefone inválido. Deve conter apenas números e ter pelo menos 9 dígitos.')
+        return telefone
+
+    # Validação de CEP
+    def clean_cep(self):
+        cep = self.cleaned_data.get('cep')
+        if not cep.isdigit() or len(cep) != 8:
+            raise ValidationError('CEP inválido. Deve conter apenas números e ter 8 dígitos.')
+        return cep
+
+    # Validação de número
+    def clean_numero(self):
+        numero = self.cleaned_data.get('numero')
+        if not numero.isdigit():
+            raise ValidationError('Número inválido. Deve conter apenas números.')
+        return numero
 
 class FuncionarioEditForm(forms.ModelForm):
     class Meta:
         model = Funcionario
         fields = ["nome", "idade", "cpf", "rg", "email", "telefone", "cep", "endereco", "bairro", "cidade", "complemento", "numero", "uf", "cargo"]
     
-    idade = forms.IntegerField(
-        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
-    )
+    # Validação de idade
+    def clean_idade(self):
+        idade = self.cleaned_data.get('idade')
+        if idade < 18:
+            raise ValidationError('A idade deve ser maior ou igual a 18 anos.')
+        return idade
 
-    cpf = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{11}$', message='O CPF deve conter exatamente 11 dígitos.')]
-    )
-
-    email = forms.CharField(
-        validators=[EmailValidator(message='Informe um endereço de e-mail válido.')]
-    )
-
-    telefone = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{9,11}$', message='Informe um número de telefone válido.')]
-    )
-
-    cep = forms.CharField(
-        validators=[RegexValidator(regex=r'^\d{8}$', message='O CEP deve conter exatamente 8 dígitos.')]
-    )
-
-    numero = forms.IntegerField(
-        validators=[RegexValidator(regex=r'^[0-9]*$', message='Apenas números são permitidos.')]
-    )
+    # Validação de CPF
+    def clean_cpf(self):
+        cpf = self.cleaned_data.get('cpf')
+        if not cpf.isdigit() or len(cpf) != 11:
+            raise ValidationError('CPF inválido. Deve conter apenas números e ter 11 dígitos.')
+        return cpf
     
-    UF = forms.CharField(
-        validators=[RegexValidator(regex=r'^[A-Za-z]{2}$', message='A UF deve conter exatamente 2 letras.')]
-    )
+    # Validação de RG
+    def clean_rg(self):
+        rg = self.cleaned_data.get('rg')
+        if not rg.isdigit() or len(rg) != 9:
+            raise ValidationError('RG inválido. Deve conter apenas números e ter 9 dígitos.')
+        return rg
+
+    # Validação de e-mail
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_validator = EmailValidator('E-mail inválido.')
+        email_validator(email)
+        return email
+
+    # Validação de telefone
+    def clean_telefone(self):
+        telefone = self.cleaned_data.get('telefone')
+        if not telefone.isdigit() or len(telefone) < 9:
+            raise ValidationError('Telefone inválido. Deve conter apenas números e ter pelo menos 9 dígitos.')
+        return telefone
+
+    # Validação de CEP
+    def clean_cep(self):
+        cep = self.cleaned_data.get('cep')
+        if not cep.isdigit() or len(cep) != 8:
+            raise ValidationError('CEP inválido. Deve conter apenas números e ter 8 dígitos.')
+        return cep
+
+    # Validação de número
+    def clean_numero(self):
+        numero = self.cleaned_data.get('numero')
+        if not numero.isdigit():
+            raise ValidationError('Número inválido. Deve conter apenas números.')
+        return numero
